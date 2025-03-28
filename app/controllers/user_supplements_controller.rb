abc5a8b01eda8951f4ad0dcd2b7c7a0c9d72d68e
Patrick Pailhes <193191145+patrick498@users.noreply.github.com>
2025-03-28 09:41:03 +0900

    Added Pundit to user_supplements controller

--- app/controllers/user_supplements_controller.rb
+++ app/controllers/user_supplements_controller.rb
@@ -1,2 +1,18 @@
 class UserSupplementsController < ApplicationController
+  include Pundit
+
+  # Rescue from unauthorized access
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  def index
+    @user_supplements = policy_scope(UserSupplement)
+
+  end
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(request.referer || root_path)
+  end
 end
